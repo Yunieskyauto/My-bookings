6919c4d7a99eba340b5f6f9b05456ec13302d849
Yuniesky Suarez Freire <yuniesky@mobai.bio>
2024-02-11 15:40:17 +0100

    adding migrations for sending rooms and restrictions

---
+++ migrations/20240211135734_seed_room_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+ ('Gneral''s Quarters','2023-11-20 00:00:00','2023-11-20 00:00:00'),
+ ('Major''s Suite','2023-11-20 00:00:00','2023-11-20 00:00:00');
